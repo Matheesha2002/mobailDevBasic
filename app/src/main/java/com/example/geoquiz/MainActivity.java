604b5f43a2168426cd374ca4a13f222823b52e8c
kavindu Matheesha <moragammanakavindu@gmail.com>
2026-02-21 23:07:23 +0530

    work shop 2

---
+++ app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.geoquiz;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mQuestionTextView;
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private Button mNextButton;
+
+    private Button mPreviousButton;
+
+
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question1, true),
+            new Question(R.string.question2, false),
+            new Question(R.string.question3, true),
+            new Question(R.string.question4, false),
+            new Question(R.string.question5, false)
+    };
+
+    private int mCurrentIndex = 0;
+    private int mCorrectAnswers = 0;
+    private int mTotalQuestions = mQuestionBank.length;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mQuestionTextView = findViewById(R.id.question_text_view);
+        mTrueButton = findViewById(R.id.true_button);
+        mFalseButton = findViewById(R.id.false_button);
+        mNextButton = findViewById(R.id.next_button);
+        mPreviousButton = findViewById(R.id.previous_button);
+
+        mPreviousButton.setOnClickListener(v -> {
+            mCurrentIndex = (mCurrentIndex - 1 + mQuestionBank.length) % mQuestionBank.length;
+            updateQuestion();
+        });
+
+        updateQuestion();
+
+        mTrueButton.setOnClickListener(v -> checkAnswer(true));
+        mFalseButton.setOnClickListener(v -> checkAnswer(false));
+
+        mNextButton.setOnClickListener(v -> {
+            mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+            updateQuestion();
+        });
+    }
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+        int messageResId;
+
+        if (userPressedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+            mCorrectAnswers++;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+
+        double successRate = ((double)mCorrectAnswers / mTotalQuestions) * 100;
+
+        Toast.makeText(this,
+                getString(messageResId) +
+                        "\nSuccess Rate: " + successRate + "%",
+                Toast.LENGTH_SHORT).show();
+    }
+}
