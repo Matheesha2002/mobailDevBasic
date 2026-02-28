1b56ce786041146c10bef2157c98dd2dc72a1ac6
kavindu Matheesha <moragammanakavindu@gmail.com>
2026-02-28 21:25:41 +0530

    get a call

--- app/src/main/java/com/example/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -1,99 +1,9 @@
-//package com.example.geoquiz;
-//import android.os.Bundle;
-//import android.widget.Button;
-//import android.widget.TextView;
-//import android.widget.Toast;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//public class MainActivity extends AppCompatActivity {
-//
-//    private TextView mQuestionTextView;
-//    private Button mTrueButton;
-//    private Button mFalseButton;
-//    private Button mNextButton;
-//
-//    private Button mPreviousButton;
-//
-//    private Button btnEnglish;
-//    private Button btnSinhala;
-//    private Button btnTamil;
-//
-//
-//
-//
-//    private Question[] mQuestionBank = new Question[] {
-//            new Question(R.string.question1, true),
-//            new Question(R.string.question2, false),
-//            new Question(R.string.question3, true),
-//            new Question(R.string.question4, false),
-//            new Question(R.string.question5, false)
-//    };
-//
-//    private int mCurrentIndex = 0;
-//    private int mCorrectAnswers = 0;
-//    private int mTotalQuestions = mQuestionBank.length;
-//
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        btnEnglish = findViewById(R.id.btnEnglish);
-//        btnSinhala = findViewById(R.id.btnSinhala);
-//        btnTamil = findViewById(R.id.btnTamil);
-//
-//        mQuestionTextView = findViewById(R.id.question_text_view);
-//        mTrueButton = findViewById(R.id.true_button);
-//        mFalseButton = findViewById(R.id.false_button);
-//        mNextButton = findViewById(R.id.next_button);
-//        mPreviousButton = findViewById(R.id.previous_button);
-//
-//        mPreviousButton.setOnClickListener(v -> {
-//            mCurrentIndex = (mCurrentIndex - 1 + mQuestionBank.length) % mQuestionBank.length;
-//            updateQuestion();
-//        });
-//
-//        updateQuestion();
-//
-//        mTrueButton.setOnClickListener(v -> checkAnswer(true));
-//        mFalseButton.setOnClickListener(v -> checkAnswer(false));
-//
-//        mNextButton.setOnClickListener(v -> {
-//            mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-//            updateQuestion();
-//        });
-//    }
-//
-//    private void updateQuestion() {
-//        int question = mQuestionBank[mCurrentIndex].getTextResId();
-//        mQuestionTextView.setText(question);
-//    }
-//
-//    private void checkAnswer(boolean userPressedTrue) {
-//        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
-//
-//        int messageResId;
-//
-//        if (userPressedTrue == answerIsTrue) {
-//            messageResId = R.string.correct_toast;
-//            mCorrectAnswers++;
-//        } else {
-//            messageResId = R.string.incorrect_toast;
-//        }
-//
-//        double successRate = ((double)mCorrectAnswers / mTotalQuestions) * 100;
-//
-//        Toast.makeText(this,
-//                getString(messageResId) +
-//                        "\nSuccess Rate: " + successRate + "%",
-//                Toast.LENGTH_SHORT).show();
-//    }
-//}
+
 package com.example.geoquiz;
 
+import android.content.Intent;
 import android.content.res.Configuration;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
@@ -115,7 +25,9 @@ public class MainActivity extends AppCompatActivity {
     private Button btnSinhala;
     private Button btnTamil;
 
-    private Question[] mQuestionBank = new Question[] {
+    private Button mCallButton;
+
+    private Question[] mQuestionBank = new Question[]{
             new Question(R.string.question1, true),
             new Question(R.string.question2, false),
             new Question(R.string.question3, true),
@@ -132,6 +44,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Call Button
+        mCallButton = findViewById(R.id.call_button);
+
         // Language Buttons
         btnEnglish = findViewById(R.id.btnEnglish);
         btnSinhala = findViewById(R.id.btnSinhala);
@@ -144,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
         mNextButton = findViewById(R.id.next_button);
         mPreviousButton = findViewById(R.id.previous_button);
 
-        // Initial Question
+        // Load first question
         updateQuestion();
 
         // TRUE button
@@ -169,9 +84,16 @@ public class MainActivity extends AppCompatActivity {
         btnEnglish.setOnClickListener(v -> setLocale("en"));
         btnSinhala.setOnClickListener(v -> setLocale("si"));
         btnTamil.setOnClickListener(v -> setLocale("ta"));
+
+        // Call Button Click (Safe Version)
+        mCallButton.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_DIAL);
+            intent.setData(Uri.parse("tel:0771234567"));
+            startActivity(intent);
+        });
     }
 
-    // Update Question Text
+    // Update Question
     private void updateQuestion() {
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
@@ -199,7 +121,7 @@ public class MainActivity extends AppCompatActivity {
                 Toast.LENGTH_SHORT).show();
     }
 
-    // Language Change Method
+    // Change Language
     private void setLocale(String langCode) {
 
         Locale locale = new Locale(langCode);
@@ -210,6 +132,6 @@ public class MainActivity extends AppCompatActivity {
 
         getResources().updateConfiguration(config, getResources().getDisplayMetrics());
 
-        recreate(); // refresh activity
+        recreate();
     }
 }
